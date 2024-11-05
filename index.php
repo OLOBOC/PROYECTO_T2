81a1b5db7e764406540bd38c03ac43b55c4d263f
OLOBOC <omarlobocuesta05@gmail.com>
2024-11-05 18:45:12 +0100

    seccion profesores y arreglo variable de $asginatura que no la tenia definida

--- index.php
+++ index.php
@@ -4,12 +4,23 @@ include_once 'clases/Profesor.php';
 include_once 'clases/Alumno.php';
 include_once 'clases/Asignatura.php';
 
+$asignaturas = ["DWES", "DWEC", "DIW", "DAW"];
+
 //llamada alumnosmuestra
 $alumnos = Alumno::crearAlumnosDeMuestra($asignaturas);
+
+//llamada profesoresmuestra
+$profesores = Profesor::crearProfesoresDeMuestra($asignaturas);
 
 // lista alumn 
 
 foreach ($alumnos as $alumno) {
     echo "Nombre: " . $alumno->getNombre() . ", Email: " . $alumno->getEmail() ;
+}
+
+// lista prof
+echo "<h2>Profesores</h2>";
+foreach ($profesores as $profesor) {
+    echo  "Nombre: " . $profesor->getNombre() . ", Email: " . $profesor->getEmail();
 }
 ?>
