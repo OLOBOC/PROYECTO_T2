b10c14fb2f10c72860a5ac7f440a293d054fc25b
OLOBOC <omarlobocuesta05@gmail.com>
2024-11-05 18:34:20 +0100

    Metodo crearAlumnosDeMuestra en la clase de alumno

--- clases/Alumno.php
+++ clases/Alumno.php
@@ -30,5 +30,25 @@ class Alumno extends Miembro
             $this->asignaturas[] = $asignatura;
         }
     }
-    
+
+    //metodo estatico crearalumnmuestra(llmar en eel index)
+    public static function crearAlumnosDeMuestra($asignaturas)
+    {
+        $alumnos = [
+            new Alumno(1, "Laura", "Martínez", "laura.martinez@email.com", 22),
+            new Alumno(2, "Sergio", "López", "sergio.lopez@email.com", 25),
+            new Alumno(3, "Carlos", "García", "carlos.garcia@email.com", 24),
+            new Alumno(4, "Marta", "Sánchez", "marta.sanchez@email.com", 23),
+            new Alumno(5, "Alba", "Fernández", "alba.fernandez@email.com", 21),
+            new Alumno(6, "David", "Rodríguez", "david.rodriguez@email.com", 26),
+            new Alumno(7, "Lucía", "Jiménez", "lucia.jimenez@email.com", 20),
+            new Alumno(8, "Jorge", "Pérez", "jorge.perez@email.com", 22),
+            new Alumno(9, "Elena", "Romero", "elena.romero@email.com", 23),
+            new Alumno(10, "Pablo", "Torres", "pablo.torres@email.com", 24),
+        ];
+
+        return $alumnos;
+    }
+
+
 }
