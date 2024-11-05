a161433d6c8e650d0a75997b52106182ace45bf4
OLOBOC <omarlobocuesta05@gmail.com>
2024-11-05 19:15:07 +0100

    matriculacion de alumnos

--- index.php
+++ index.php
@@ -42,3 +42,22 @@ echo "<h2>Alumnos <= 23</h2>";
 foreach ($filtradoalumn as $alumn) {
     echo "<li>" . "Nombre: " . $alumn->getNombre() . ", Email: " . $alumn->getEmail() . "</li>";
 }
+
+//matriculacion de alumnos en asignaturas
+$alumnos[0]->matricularEnAsignatura($asignaturas[0]);
+$alumnos[1]->matricularEnAsignatura($asignaturas[0]);
+$alumnos[1]->matricularEnAsignatura($asignaturas[1]);
+$alumnos[2]->matricularEnAsignatura($asignaturas[0]);
+$alumnos[2]->matricularEnAsignatura($asignaturas[2]);
+$alumnos[3]->matricularEnAsignatura($asignaturas[0]);
+$alumnos[4]->matricularEnAsignatura($asignaturas[0]);
+$alumnos[4]->matricularEnAsignatura($asignaturas[1]);
+$alumnos[4]->matricularEnAsignatura($asignaturas[2]);
+$alumnos[5]->matricularEnAsignatura($asignaturas[0]);
+$alumnos[6]->matricularEnAsignatura($asignaturas[1]);
+$alumnos[6]->matricularEnAsignatura($asignaturas[1]);
+$alumnos[7]->matricularEnAsignatura($asignaturas[2]);
+$alumnos[8]->matricularEnAsignatura($asignaturas[1]);
+$alumnos[9]->matricularEnAsignatura($asignaturas[0]);
+
+?>
