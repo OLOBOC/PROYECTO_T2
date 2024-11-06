e7e6b286f50f5f9d6aaf6af6f925301bac1919eb
Omar <omarlobocuesta05@gmail.com>
2024-11-06 08:42:42 +0100

    llamada al triangulo

--- clases/TriangleGenerator.php
+++ clases/TriangleGenerator.php
@@ -4,14 +4,16 @@ class Trianglulo {
 
    
     public function generartriangulo(int $altura): string {
-        
-
+        // cadena vacia por si es negativo
+        if ($altura < 0) {
+            return "Error es negativo";
+        }
         $triangulo = ""; // Cadena para almacenar el triángulo completo
 
         // Bucle para las filas del triángulo
         for ($i = 0; $i < $altura; $i++) {
             // Calculo de los espacios 
-            $spacios = str_repeat("&nbsp;", $altura - $i - 1);
+            $spacios = str_repeat("&nbsp;", $altura - $i - 1); //con sqrt calculo la reiz cuadrada y repito
 
             // Calculo asteriscos
             $asteriscos= str_repeat("*", $i * 2 + 1);
---
+++ triangle.php
@@ -0,0 +1,6 @@
+<?php
+include "clases/triangleGenerator.php";
+ 
+ $triangulo = new Trianglulo();
+ echo $triangulo->generartriangulo(6);
+ ?>
